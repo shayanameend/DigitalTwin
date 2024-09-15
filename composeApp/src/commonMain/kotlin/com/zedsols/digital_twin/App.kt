8cd90145352b773bde89d59328c904e90d31f728
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-09-15 09:50:17 +0500

    Updated Material UI 2 to 3 and added Material Extended Icons. Added LoginScreen Composable.

--- composeApp/src/commonMain/kotlin/com/zedsols/digital_twin/App.kt
+++ composeApp/src/commonMain/kotlin/com/zedsols/digital_twin/App.kt
@@ -1,36 +1,60 @@
 package com.zedsols.digital_twin
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import digitaltwin.composeapp.generated.resources.Res
-import digitaltwin.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@Composable
 @Preview
+@Composable
 fun App() {
   MaterialTheme {
-    var showContent by remember { mutableStateOf(false) }
-    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-      Button(onClick = { showContent = !showContent }) {
-        Text("Click me!")
-      }
-      AnimatedVisibility(showContent) {
-        val greeting = remember { Greeting().greet() }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-          Image(painterResource(Res.drawable.compose_multiplatform), null)
-          Text("Compose: $greeting")
-        }
-      }
+    Column(
+      Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      LoginScreen()
+    }
+  }
+}
+
+@Preview
+@Composable
+fun LoginScreen() {
+  var email by remember { mutableStateOf("") }
+  var password by remember { mutableStateOf("") }
+
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    TextField(
+      value = email,
+      onValueChange = { email = it },
+      label = { Text("Email") }
+    )
+    TextField(
+      value = password,
+      onValueChange = { password = it },
+      label = { Text("Password") },
+      visualTransformation = PasswordVisualTransformation()
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    Button(
+      shape = MaterialTheme.shapes.small,
+      onClick = { println("Email: $email, Password: $password") },
+    ) {
+      Text("Login")
+    }
+    TextButton(
+      onClick = { println("Navigate to Sign-Up") }) {
+      Text("Don't have an account? Sign Up")
     }
   }
 }
